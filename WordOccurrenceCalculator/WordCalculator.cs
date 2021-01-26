743381e293e4c6dff2adf0ecc96376998ed23506
treymeares <59393122+treymeares@users.noreply.github.com>
2021-01-26 15:51:52 -0500

    Addded code for CalculateOccurrences

--- WordOccurrenceCalculator/WordCalculator.cs
+++ WordOccurrenceCalculator/WordCalculator.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
 
 namespace WordOccurrenceCalculator
 {
     class WordCalculator
     {
+        public static WordOccurrence[] CalculateOccurrences(ArrayList inputList)
+        {
+            try
+            {
+                WordOccurrence[] obj = new WordOccurrence[inputList.Count];
+                for(int i = 0; i < inputList.Count; i++)
+                {
+                    obj[i] = new WordOccurrence();
+                    obj[i].Word = inputList[i].ToString();
+
+                    //counting the occurrences of the word
+                    
+                    for (int j = 0; i < inputList.Count; j++)
+                    {
+                        String countWord = inputList.ToString();
+                        if (obj[i].Word == countWord)
+                            obj[i].Count++;
+                    }
+                }
+                return obj;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
