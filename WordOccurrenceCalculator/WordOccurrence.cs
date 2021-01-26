deecb22b52a53a0c5ebcf19d1045d7f03d2d0415
treymeares <59393122+treymeares@users.noreply.github.com>
2021-01-26 15:34:33 -0500

    Added WordOccurrence class

---
+++ WordOccurrenceCalculator/WordOccurrence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordOccurrenceCalculator
+{
+    class WordOccurrence
+    {
+        public string Word;
+        public int Count;
+    }
+}
